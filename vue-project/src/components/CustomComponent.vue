f4076b107d4334bbbc9352402c807c0e4824adea
Bogdan <bogdan1850@gmail.com>
2022-09-27 00:52:07 +0200

    done

---
+++ vue-project/src/components/CustomComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>Hello, my name is {{name}}</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+<script setup>
+const name = "Bogdan"
+</script>
+
+
+<style>
+
+</style>
